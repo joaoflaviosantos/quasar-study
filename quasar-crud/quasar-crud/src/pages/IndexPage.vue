b7178d36049169b6391dd268a37247a0fab19b99
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-05-06 13:24:18 -0300

    feat: First commit, initial project configs

---
+++ quasar-crud/quasar-crud/src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md">
+      <q-table
+        title="Posts"
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+
+  }
+})
+</script>
