96a89bd3b3a1eda8880b21a35ae9f87447ad64c8
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-05-09 13:03:56 -0300

    feat: Implementing fist axios request on /posts

--- quasar-crud/frontend/src/pages/IndexPage.vue
+++ quasar-crud/frontend/src/pages/IndexPage.vue
@@ -1,13 +1,19 @@
 <template>
   <q-page padding>
     <div class="q-pa-md">
-      <q-table title="Posts" :rows="posts" :columns="columns" row-key="name" />
+      <q-table
+      title="Posts"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+      />
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
+import { api } from 'boot/axios'
 
 export default defineComponent({
   name: "IndexPage",
@@ -15,10 +21,24 @@ export default defineComponent({
     const posts = ref([]);
 
     const columns = [
-      { name: 'id', field: 'id', label: 'Id', sortable: true, align: 'left'},
+      { name: 'id', field: 'id', label: 'Id', sortable: true, align: 'left' },
       { name: 'title', field: 'title', label: 'Title', sortable: true, align: 'left' },
       { name: 'author', field: 'author', label: 'Author', sortable: true, align: 'left' },
-    ]
+      { name: 'actions', field: 'actions', label: 'Actions', sortable: false, align: 'left' }
+    ];
+
+    onMounted(() => {
+      getPost();
+    });
+
+    const getPost = async () => {
+      try {
+        const { data } = await api.get('posts');
+        posts.value = data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     return {
       posts,
