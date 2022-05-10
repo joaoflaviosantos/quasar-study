e3a22a7bd832d3464fee66efc5f5e32f02401566
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-05-10 13:28:47 -0300

    feat: Implementing delete/remove function to posts table

--- quasar-crud/frontend/src/pages/IndexPage.vue
+++ quasar-crud/frontend/src/pages/IndexPage.vue
@@ -6,7 +6,14 @@
       :rows="posts"
       :columns="columns"
       row-key="name"
-      />
+      >
+      <!--<template v-slot:body-cell-actions="props">-->
+      <template #body-cell-actions="props">
+      <q-td :props="props">
+      <q-btn icon="delete" color="negative" dense size="sm" @click="handleDeletePost(props.row.id)"/>
+      </q-td>
+      </template>
+      </q-table>
     </div>
   </q-page>
 </template>
@@ -14,20 +21,24 @@
 <script>
 import { defineComponent, ref, onMounted } from "vue";
 import postsService from "src/services/posts"
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
   name: "IndexPage",
   setup() {
-    const posts = ref([]);
+
+    const $q = useQuasar()
 
     const columns = [
       { name: 'id', field: 'id', label: 'Id', sortable: true, align: 'left' },
       { name: 'title', field: 'title', label: 'Title', sortable: true, align: 'left' },
       { name: 'author', field: 'author', label: 'Author', sortable: true, align: 'left' },
-      { name: 'actions', field: 'actions', label: 'Actions', sortable: false, align: 'left' }
+      { name: 'actions', field: 'actions', label: 'Actions', sortable: false, align: 'center' }
     ];
 
-    const { list } = postsService();
+    const posts = ref([]);
+
+    const { list, remove } = postsService();
 
     const getPost = async () => {
       try {
@@ -42,10 +53,31 @@ export default defineComponent({
       getPost();
     });
 
+    const handleDeletePost = async (id) => {
+      try {
+
+        $q.dialog({
+        title: 'Confirm',
+        message: `Do you really want to delete Post ${id}?`,
+        cancel: true,
+        persistent: true
+        }).onOk(async () => {
+        await remove(id);
+        $q.notify({ message: `Post ${id} deleted!`, color: 'positive', icon: 'check' })
+        await getPost()
+        })
+      } catch (error) {
+        //alert(error);
+        $q.notify({ message: `Error on delete Post ${id}`, color: 'negative', icon: 'times' })
+      }
+    }
+
     return {
       posts,
-      columns
+      columns,
+      handleDeletePost
     };
+
   },
 });
 </script>
